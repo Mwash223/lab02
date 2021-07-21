68f558a86644cd322f0173916b12ed6b2a08ee77
Malik Washington <kingmalikwashington@gmail.com>
2021-07-21 15:43:44 -0500

    push for lab2

---
+++ script.js
@@ -0,0 +1,24 @@
+var name=prompt("What is Maliks' fan name");
+alert('Hello ' + name);
+
+
+    var answer1 = prompt(name + " Is Malik's date of birth Sept 9th");
+
+
+
+if(answer1 == "Yes"){
+    alert("Is Correct");
+} else if (answer1 == "No"){
+    alert("Wrong");
+}
+
+var answer2 = prompt(name + " Is Malik in CodeCrew");
+
+if(answer2 =="Yes"){
+    alert("Correct")
+}
+else if (answer2 == "No"){
+    alert("Wrong");}
+    
+    
+
