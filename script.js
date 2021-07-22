59a5f321df75959a39853dd5b131957cbe680abd
Malik Washington <kingmalikwashington@gmail.com>
2021-07-22 09:47:56 -0500

    I added to my paragraph in my html file

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-var name=prompt("What is Maliks' fan name");
+let name=prompt("What is Maliks' fan name");
 alert('Hello ' + name);
 
 
-    var answer1 = prompt(name + " Is Malik's date of birth Sept 9th");
+    let answer1 = prompt(name + " Is Malik's date of birth Sept 9th");
 
 
 
-if(answer1 == "Yes"){
+if(answer1.toLocaleUpperCase == "Yes"){
     alert("Is Correct");
 } else if (answer1 == "No"){
     alert("Wrong");
 }
 
-var answer2 = prompt(name + " Is Malik in CodeCrew");
+let answer2 = prompt(name + " Is Malik in CodeCrew");
 
-if(answer2 =="Yes"){
+if(answer2.toLowerCase =="Yes"){
     alert("Correct")
 }
 else if (answer2 == "No"){
